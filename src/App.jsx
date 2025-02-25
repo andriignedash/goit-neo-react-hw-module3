4347768d4adb88d0c76bc864093cfd185c6b7fe4
andriignedash <116560584+andriignedash@users.noreply.github.com>
2025-02-25 21:52:58 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+import Description from "./components/Description/Description";
+import Feedback from "./components/Feedback/Feedback";
+import Options from "./components/Options/Options";
+import Notification from "./components/Notification/Notification";
+import "./App.css";
+
+const App = () => {
+  const [feedback, setFeedback] = useState(() => {
+    return (
+      JSON.parse(localStorage.getItem("feedback")) || {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+      }
+    );
+  });
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prevState) => ({
+      ...prevState,
+      [feedbackType]: prevState[feedbackType] + 1,
+    }));
+  };
+
+  const resetFeedback = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positiveFeedback = totalFeedback
+    ? Math.round((feedback.good / totalFeedback) * 100)
+    : 0;
+
+  return (
+    <div className="appContainer">
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        resetFeedback={resetFeedback}
+        totalFeedback={totalFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback
+          feedback={feedback}
+          total={totalFeedback}
+          positive={positiveFeedback}
+        />
+      ) : (
+        <Notification message="No feedback yet. Be the first to leave a review!" />
+      )}
+    </div>
+  );
+};
+
+export default App;
